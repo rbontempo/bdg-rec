964c5d119e8b2b5ef83723b706ed8694b3ae6761
Renato Bontempo <rbontempo@gmail.com>
2026-03-14 12:29:44 -0300

    chore: add .gitignore, remove build/ from tracking

--- build/CMakeFiles/BDG_REC.dir/_deps/juce-src/modules/juce_core/juce_core_CompilationTime.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/BDG_REC.dir/_deps/juce-src/modules/juce_core/juce_core_CompilationTime.cpp.o: \
-  /Users/rbontempo/Documents/AI\ -\ antigravity/bdg-rec-juce/build/_deps/juce-src/modules/juce_core/juce_core_CompilationTime.cpp
